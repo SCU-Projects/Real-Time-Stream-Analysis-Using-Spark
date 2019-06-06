6d6801226ee59821b4a980577006bab828ab60cd
Reshma Subramaniam <hi2resh26@gmail.com>
2019-06-06 03:45:00 -0700

    Computed average and mean for the Octet field with tumbling window.

--- src/main/java/RealTimeStreamAnalysisDriver.java
+++ src/main/java/RealTimeStreamAnalysisDriver.java
@@ -1,11 +1,7 @@
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.Optional;
 import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import scala.Tuple2;
 
 import java.util.*;
 import java.util.logging.Level;
@@ -13,34 +9,55 @@ import java.util.logging.Logger;
 
 public class RealTimeStreamAnalysisDriver {
 
+    static Long expiryTime = null;
+    static Queue<Long> processQueue = new LinkedList<>();
+
     public static void main(String[] args) throws Exception {
-        // Configure and initialize the SparkStreamingContext
         SparkConf conf = new SparkConf()
                 .setMaster("local[*]")
                 .setAppName("SparkRealTimeStreamingApp");
         JavaStreamingContext streamingContext =
-                new JavaStreamingContext(conf, Durations.seconds(5));
+                new JavaStreamingContext(conf, Durations.seconds(20));
         Logger.getAnonymousLogger().setLevel(Level.SEVERE);
-
-        // Receive streaming data from the source
         JavaReceiverInputDStream<String> udpPacket = streamingContext.socketTextStream(Constants.HOST, Constants.PORT);
         List<Long> octetsList = new ArrayList<>();
-        udpPacket.foreachRDD(packet -> octetsList.add(Long.parseLong(packet.toString().split(",")[16])));
-        Long max = Long.MIN_VALUE;
-        Long mean = Long.valueOf(0);
-        for(Long octet : octetsList){
-            System.out.println(octet+"\t");
-            if(max < octet)
-                max = octet;
-            mean += octet;
-        }
-        mean /= octetsList.size();
-        System.out.println("Max:" + max + "\tAvg:" + mean);
-        // Execute the Spark workflow defined above
+        udpPacket.foreachRDD(packetRdd -> processOctetList(packetRdd.collect()));
         streamingContext.start();
-        streamingContext.awaitTerminationOrTimeout(15000);
-        streamingContext.stop();
+        streamingContext.awaitTermination();
+
     }
 
+    static void processOctetList(List<String> packetList) {
+        System.out.println("Size" + packetList.size());
+        for (String packet : packetList) {
+            Long currentTime = Long.parseLong(packet.split(",")[4]);
+            if (expiryTime == null)
+                expiryTime = currentTime + Long.valueOf(60000);
+            if (expiryTime <= currentTime) {
+                getMaxAndAvgForQueue(processQueue, processQueue.size());
+                expiryTime += Long.valueOf(60000);
+            }
+            processQueue.add(Long.parseLong(packet.split(",")[15]));
+        }
+    }
 
+    static void getMaxAndAvgForQueue(Queue<Long> toProcessQueue, Integer size) {
+
+        Integer queueSize = size;
+        if (size == 0)
+            return;
+        Long max = Long.MIN_VALUE;
+        Long mean = Long.valueOf(0);
+        while (size > 0) {
+            Long octet = toProcessQueue.remove();
+            if (max < octet)
+                max = octet;
+            mean += octet;
+            size--;
+        }
+        mean /= queueSize;
+        System.out.println("Max:" + max + "\tAvg:" + mean);
+
+    }
 }
+
